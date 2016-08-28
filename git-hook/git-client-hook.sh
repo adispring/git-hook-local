ff6122074faca4bbda907c4bdae44408837559af
wangzengdi <sunnyadi@163.com>
2016-08-28 10:01:34 +0800

    optimize git install script

--- git-hook/git-client-hook.sh
+++ git-hook/git-client-hook.sh
@@ -9,28 +9,51 @@ HOOK_FILE_NAMES=$(ls ${HOOK_REPO_DIR} | grep -E "\.hook$")
 if [ ! -d "$HOOK_DIR" ]; then
   echo "No ${HOOK_DIR} exist!"
 else
-  echo "GIT LOCAL HOOK installing...! ⚙ "
+  ALL_HOOKS=()
+  EXIST_HOOKS=()
+  UPDATE_HOOKS=()
+  INSTALL_HOOKS=()
   for hook_file in ${HOOK_FILE_NAMES}
   do
+    ALL_HOOKS[${#ALL_HOOKS[@]}]=${hook_file}
     file=${hook_file%.hook}
     if [ -f ${HOOK_DIR}${file} ]; then
       file_diff=$(diff ${HOOK_REPO_DIR}${hook_file} ${HOOK_DIR}${file})
       
       if [ -z "$file_diff" ]; then
-        echo "${file} exists, and nothing changed."
+        EXIST_HOOKS[${#EXIST_HOOKS[@]}]=${hook_file}
       else
+        UPDATE_HOOKS[${#UPDATE_HOOKS[@]}]=${hook_file}
+      fi
+    else
+      INSTALL_HOOKS[${#INSTALL_HOOKS[@]}]=${hook_file}
+    fi
+  done
+
+  if [ ${#EXIST_HOOKS[@]} -eq ${#ALL_HOOKS[@]} ]; then
+    echo "No git hook need to update or install."
+  else
+    echo -e "GIT LOCAL HOOK installing...! ⚙ \n"
+    if [ ${#UPDATE_HOOKS[@]} -gt 0 ]; then
+      for hook_file in ${UPDATE_HOOKS}
+      do
+        file=${hook_file%.hook}
+        file_diff=$(diff ${HOOK_REPO_DIR}${hook_file} ${HOOK_DIR}${file})
         echo -e "${file} has changed: \n${file_diff}"
         echo "${file} updating..."
         cp ${HOOK_REPO_DIR}${hook_file} ${HOOK_DIR}${file}
-        echo "${file} updated!"
-      fi
-    else
-      echo "${file} installing..."
-      cp ${HOOK_REPO_DIR}${hook_file} ${HOOK_DIR}${file}
-      echo "${file} installed!"
+        echo -e "${file} updated!\n"
+       done
     fi
-  done
-  echo "GIT LOCAL HOOK install done!  🍻"
+    if [ ${#INSTALL_HOOKS[@]} -gt 0 ]; then
+      for hook_file in ${INSTALL_HOOKS}
+      do
+        file=${hook_file%.hook}
+        echo "${file} installing..."
+        cp ${HOOK_REPO_DIR}${hook_file} ${HOOK_DIR}${file}
+        echo -e "${file} intsalled!\n"
+       done
+    fi
+    echo "GIT LOCAL HOOK install done!  🍻"
+  fi 
 fi
-
-exit 0
