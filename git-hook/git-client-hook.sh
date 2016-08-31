e6c24808fc1b954ba75283360fd6c3a6d00157fb
wangzengdi <sunnyadi@163.com>
2016-08-31 16:40:13 +0800

    fix variable to string error

--- git-hook/git-client-hook.sh
+++ git-hook/git-client-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n ${NODE_ENV} -a ${NODE_ENV} != "development" ]; then
+if [ -n "$NODE_ENV" -a "$NODE_ENV" != "development" ]; then
   echo "No need to install git-hook in NODE_ENV: ${NODE_ENV}."
   exit 0
 fi
