39a1cfc2079e9923115d31b3f0df35684ae0cc93
wangzengdi <sunnyadi@163.com>
2016-09-09 11:49:26 +0800

    optimize setup and teardown

--- test/pre-commit.bats
+++ test/pre-commit.bats
@@ -1,20 +1,24 @@
 #!/usr/bin/env bats
 load test_helper
 
-@test "is_valid_email should exit 1 if email format is invalid." {
+setup() {
+  mkdir -p "$HOOK_TEST_PATH"
   cd "$HOOK_TEST_PATH"
+  cp -rf "$INSTALL_SCRIPT_PATH" "$HOOK_TEST_PATH"
+}
+
+@test "pre-commit should fail if email format is invalid, and vice versa." {
   git init
   git config user.email sunnyadi@163.com
   run pre-commit
+  assert_failure
   assert_line 0 "Git commit FAILED! ❌ "
   assert_line 1 "Git user.email: sunnyadi@163.com is invalid, please use company's email! ✏️ "
 }
 
-@test "is_valid_email should exit 0 if email format is valid." {
-  cd "$HOOK_TEST_PATH"
+@test "pre-commit should exit 0 if email format is valid." {
   git init
   git config user.email wangzengdi@meituan.com
   run pre-commit
   assert_success
 }
-
--- test/test_helper.bash
+++ test/test_helper.bash
@@ -7,21 +7,22 @@ export TESTS_PATH="$BATS_TEST_DIRNAME"
 export PROJECT_PATH="$(cd "$TESTS_PATH/.." && pwd)"
 export INSTALL_SCRIPT_PATH="$PROJECT_PATH/git-hook"
 
-setup() {
-  if [ -z "$HOOK_TEST_PATH" ]; then
-    HOOK_TEST_PATH="$HOME/git-hook-local-temp"
-    export HOOK_TEST_PATH="$(mktemp -d "${HOOK_TEST_PATH}.XXX" 2>/dev/null || echo "$HOOK_TEST_PATH")"
-    echo $HOOK_TEST_PATH
-    cp -rf "$INSTALL_SCRIPT_PATH" "$HOOK_TEST_PATH"
-
-    export HOOK_TEST_INSTALL_PATH="$HOOK_TEST_PATH/git-hook"
-    export HOOK_TEST_REPO_PATH="$HOOK_TEST_PATH/git-hook/hooks"
-
-    PATH="$HOOK_TEST_INSTALL_PATH:$PATH"
-    PATH="$HOOK_TEST_REPO_PATH:$PATH"
-    export PATH
+if [ -z "$HOOK_TEST_PATH" ]; then
+  if [ -n "$TMPDIR" ]; then
+    HOOK_TEST_PATH="$TMPDIR/git-hook-local-temp"
+  else
+    HOOK_TEST_PATH="$HOME/tmp/git-hook-local-temp"
   fi
-}
+  export HOOK_TEST_PATH="$(mktemp -d "${HOOK_TEST_PATH}.XXX" 2>/dev/null || echo "$HOOK_TEST_PATH")"
+  cp -rf "$INSTALL_SCRIPT_PATH" "$HOOK_TEST_PATH"
+
+  export HOOK_TEST_INSTALL_PATH="$HOOK_TEST_PATH/git-hook"
+  export HOOK_TEST_REPO_PATH="$HOOK_TEST_PATH/git-hook/hooks"
+
+  PATH="$HOOK_TEST_INSTALL_PATH:$PATH"
+  PATH="$HOOK_TEST_REPO_PATH:$PATH"
+  export PATH
+fi
 
 teardown() {
   rm -rf "$HOOK_TEST_PATH"
